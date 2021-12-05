f205a3a20596c93f02b58984482635fedbdbe96e
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-12-05 17:04:27 +0100

    added leaflet map w/ mapbox public token

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <Map msg="Welcome Habibi, do you happen see a map?"/>
+
+  <!-- Map -->
+  <div class="map" id="mapid">
+    <l-map 
+      v-model="zoom"
+      v-model:zoom="zoom"
+      :center="[47.41322, -1.219482]"
+      @move="log('move')">
+      <l-geo-json :geojson="geojson" :options="geojsonOptions" />
+    </l-map>
+  </div>
 </template>
 
 <script>
+// DON'T load Leaflet components here!
+// Its CSS is needed though, if not imported elsewhere in your application.
+import { LMap, LGeoJson } from "@vue-leaflet/vue-leaflet"
+import "leaflet/dist/leaflet.css"
+
+// John tutorial
+import leaflet from "leaflet"
+
 import Map from './components/Map.vue'
+import { onMounted } from '@vue/runtime-core'
+// Not sure which import, above or below, is correct
+// import { onMounted } from 'vue'
+
 
 export default {
   name: 'App',
   components: {
-    Map
-  }
-}
+    Map,
+    LMap,
+    LGeoJson,
+  },
+  setup() {
+    let mymap;
+
+    onMounted(() => {
+      mymap = leaflet.map('mapid').setView([51.505, -0.09], 13);
+
+      leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGVlcGl0eSIsImEiOiJja3d0ZXF5OXMxZm5wMnBxbzdoNXJ6bHRqIn0.a2pkiB6fYydkqkzqFstnHA', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoiZGVlcGl0eSIsImEiOiJja3d0ZXF5OXMxZm5wMnBxbzdoNXJ6bHRqIn0.a2pkiB6fYydkqkzqFstnHA'
+}).addTo(mymap);
+    })
+  },
+};
 </script>
 
 <style>
@@ -23,4 +64,9 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.map { 
+  height: 75vh;
+  width: 75vw;
+  margin: 0 auto;
+  }
 </style>
